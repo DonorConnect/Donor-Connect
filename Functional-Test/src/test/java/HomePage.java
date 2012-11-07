186d5ae2ac96355e2eeec9e4837bd7f3d5bd4145
pragya <pragya.91.jain+git@gmail.com>
2012-11-07 15:40:50 +0530

    pragya/sailee/amir - removed comments

--- Functional-Test/src/test/java/HomePage.java
+++ Functional-Test/src/test/java/HomePage.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class HomePage {
@@ -17,7 +16,6 @@ public class HomePage {
     public void BeforeTest() {
         wd = new HtmlUnitDriver();
         wd.get("http://10.10.4.121:8080/Donor-Connect-App/");
-//     wd.manage().window().maximize();
     }
 
     @Test
@@ -27,13 +25,8 @@ public class HomePage {
         welcomeText = wd.findElement(By.xpath("//h1"));
         welcomeImage = wd.findElement(By.xpath("//img[@src='image/children.jpg']"));
 
-        //logo.isDisplayed();
         Assert.assertEquals(welcomeText.getText(), "welcome...");
         welcomeImage.isDisplayed();
     }
-
-    //@Test
-    //public void HomePage
-
 }
 
