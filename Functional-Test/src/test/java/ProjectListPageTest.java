ff91b2911bb1efafbf3e3ca89c38aa7fa3c8bfb8
pragya <pragya.91.jain+git@gmail.com>
2012-11-07 16:02:08 +0530

    pragya/sailee/amir - moved webdriver initialization to setup, renamed from wd to webDriver

--- Functional-Test/src/test/java/ProjectListPageTest.java
+++ Functional-Test/src/test/java/ProjectListPageTest.java
@@ -1,4 +1,5 @@
 import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -12,30 +13,35 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.junit.Assert.assertTrue;
 
 public class ProjectListPageTest {
-    WebDriver wd = new HtmlUnitDriver();
+    WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        webDriver = new HtmlUnitDriver();
+    }
 
     @Test
     public void clickableSeeAll() {
-        wd.get("http://10.10.4.121:8080/Donor-Connect-App/home.ftl");
-        wd.findElement(By.xpath("//a[@href='all_projects.ftl']")).isEnabled();
-        wd.findElement(By.xpath("//a[@href='all_projects.ftl']")).click();
-        Assert.assertEquals("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl", wd.getCurrentUrl());
+        webDriver.get("http://10.10.4.121:8080/Donor-Connect-App/home.ftl");
+        webDriver.findElement(By.xpath("//a[@href='all_projects.ftl']")).isEnabled();
+        webDriver.findElement(By.xpath("//a[@href='all_projects.ftl']")).click();
+        Assert.assertEquals("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl", webDriver.getCurrentUrl());
     }
 
     @Test
     public void checkLogoAndHeadline() {
-        wd.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
-        //Assert.assertEquals("http://localhost:8080/Donor-Connect-App/all_projects.ftl",wd.getCurrentUrl());
-        Assert.assertTrue(wd.findElement(By.className("logopic")).isDisplayed());
-        Assert.assertTrue(wd.findElement(By.xpath("//div[@class='projectHeader']/h3")).isDisplayed());
+        webDriver.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
+        //Assert.assertEquals("http://localhost:8080/Donor-Connect-App/all_projects.ftl",webDriver.getCurrentUrl());
+        Assert.assertTrue(webDriver.findElement(By.className("logopic")).isDisplayed());
+        Assert.assertTrue(webDriver.findElement(By.xpath("//div[@class='projectHeader']/h3")).isDisplayed());
     }
 
     @Test
     public void clickableThumbnail() {
-        wd.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
-        if (wd.findElement(By.xpath("(//img)[2]")).isDisplayed()) {
-            wd.findElement(By.xpath("(//img)[2]")).click();
-            Assert.assertTrue(wd.getCurrentUrl().contains("http://10.10.4.121:8080/Donor-Connect-App/project_detail.ftl"));
+        webDriver.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
+        if (webDriver.findElement(By.xpath("(//img)[2]")).isDisplayed()) {
+            webDriver.findElement(By.xpath("(//img)[2]")).click();
+            Assert.assertTrue(webDriver.getCurrentUrl().contains("http://10.10.4.121:8080/Donor-Connect-App/project_detail.ftl"));
         } else {
             System.out.println("No projects");
         }
@@ -43,10 +49,10 @@ public class ProjectListPageTest {
 
     @Test
     public void clickableProjectName() {
-        wd.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
-        if (wd.findElement(By.xpath("(//a[@class='projectName'])[1]")).isDisplayed()) {
-            wd.findElement(By.xpath("(//a[@class='projectName'])[1]")).click();
-            Assert.assertTrue(wd.getCurrentUrl().contains("http://10.10.4.121:8080/Donor-Connect-App/project_detail.ftl"));
+        webDriver.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
+        if (webDriver.findElement(By.xpath("(//a[@class='projectName'])[1]")).isDisplayed()) {
+            webDriver.findElement(By.xpath("(//a[@class='projectName'])[1]")).click();
+            Assert.assertTrue(webDriver.getCurrentUrl().contains("http://10.10.4.121:8080/Donor-Connect-App/project_detail.ftl"));
         } else {
             System.out.println("No projects");
         }
@@ -54,11 +60,11 @@ public class ProjectListPageTest {
 
     @Test
     public void checkProjectName() {
-        wd.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
-        if (wd.findElement(By.xpath("(//a[@class='projectName'])[1]")).isDisplayed()) {
-            String project_name = wd.findElement(By.xpath("(//a[@class='projectName'])[1]")).getText();
-            wd.findElement(By.xpath("(//a[@class='projectName'])[1]")).click();
-            Assert.assertEquals(wd.findElement(By.xpath("//h2[@class='project-name']")).getText(), project_name);
+        webDriver.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
+        if (webDriver.findElement(By.xpath("(//a[@class='projectName'])[1]")).isDisplayed()) {
+            String project_name = webDriver.findElement(By.xpath("(//a[@class='projectName'])[1]")).getText();
+            webDriver.findElement(By.xpath("(//a[@class='projectName'])[1]")).click();
+            Assert.assertEquals(webDriver.findElement(By.xpath("//h2[@class='project-name']")).getText(), project_name);
         } else {
             System.out.println("No projects");
         }
@@ -66,36 +72,36 @@ public class ProjectListPageTest {
 
 
     public void insertDataAndCheck(String name, String description, String image, String thumbnail, String summary, String status) {
-        wd.get("http://10.10.4.121:8080/Donor-Connect-App/inject_project.ftl");
-        wd.findElement(By.name("name")).sendKeys(name);
-        wd.findElement(By.name("description")).sendKeys(description);
-        wd.findElement(By.name("img")).sendKeys(image);
+        webDriver.get("http://10.10.4.121:8080/Donor-Connect-App/inject_project.ftl");
+        webDriver.findElement(By.name("name")).sendKeys(name);
+        webDriver.findElement(By.name("description")).sendKeys(description);
+        webDriver.findElement(By.name("img")).sendKeys(image);
         if (status.equalsIgnoreCase("Complete")) {
-            Select foo = new Select(wd.findElement(By.name("status")));
+            Select foo = new Select(webDriver.findElement(By.name("status")));
             foo.selectByValue("COMPLETED");
-            // wd.findElement(By.name("status")).sendKeys(Keys.ARROW_DOWN);
+            // webDriver.findElement(By.name("status")).sendKeys(Keys.ARROW_DOWN);
         }
-        wd.findElement(By.name("thumbnail")).sendKeys(thumbnail);
-        wd.findElement(By.name("summary")).sendKeys(summary);
-        wd.findElement(By.xpath("//div[@id='submit_button']/input")).click();
+        webDriver.findElement(By.name("thumbnail")).sendKeys(thumbnail);
+        webDriver.findElement(By.name("summary")).sendKeys(summary);
+        webDriver.findElement(By.xpath("//div[@id='submit_button']/input")).click();
 
-        WebElement element = (new WebDriverWait(wd, 4)).until(ExpectedConditions.visibilityOfElementLocated(By.id("project_id")));
+        WebElement element = (new WebDriverWait(webDriver, 4)).until(ExpectedConditions.visibilityOfElementLocated(By.id("project_id")));
         if (!element.isDisplayed()) {
             System.exit(0);
         }
-        //String project_id = wd.findElement(By.id("project_id")).getText();
+        //String project_id = webDriver.findElement(By.id("project_id")).getText();
     }
 
     public void verifyPositive(String name, String thumbnail, String summary) {
-        wd.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
-        Assert.assertEquals((wd.findElement(By.xpath("//div[@class='eachProject'][last()]/div/h4/a[@class='projectName']")).getText()), name);
-        Assert.assertEquals((wd.findElement(By.xpath("//div[@class='eachProject'][last()]/div/p[@class='projectBrief']")).getText()), summary);
-        assertTrue(wd.findElement(By.xpath("//div[@class='eachProject'][last()]/div/a/img[@src='" + thumbnail + "']")).isDisplayed());
+        webDriver.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
+        Assert.assertEquals((webDriver.findElement(By.xpath("//div[@class='eachProject'][last()]/div/h4/a[@class='projectName']")).getText()), name);
+        Assert.assertEquals((webDriver.findElement(By.xpath("//div[@class='eachProject'][last()]/div/p[@class='projectBrief']")).getText()), summary);
+        assertTrue(webDriver.findElement(By.xpath("//div[@class='eachProject'][last()]/div/a/img[@src='" + thumbnail + "']")).isDisplayed());
     }
 
     public boolean isElementPresent(By by) {
         try {
-            wd.findElement(by);
+            webDriver.findElement(by);
             return true;
         } catch (NoSuchElementException e) {
             return false;
@@ -103,7 +109,7 @@ public class ProjectListPageTest {
     }
 
     public void verifyNegative(String name, String thumbnail, String summary) {
-        wd.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
+        webDriver.get("http://10.10.4.121:8080/Donor-Connect-App/all_projects.ftl");
         boolean x = isElementPresent(By.xpath("//div[@class='eachProject'][last()]/div/h4/a[@class='projectName']"));
         if (!x) {
             System.out.println("Test Successful");
