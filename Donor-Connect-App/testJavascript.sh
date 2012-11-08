7612199a8e8f61a3db9dc1eacee514ac7e91d89e
Longkai Cao <lkcao@thoughtworks.com>
2012-11-08 15:49:41 +0530

    Shari/Longkai Story #14 - modify the test js script to include full path of phantomjs

--- Donor-Connect-App/testJavascript.sh
+++ Donor-Connect-App/testJavascript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #if [ $1 -eq 'mac']
-phantomjs src/test/javascript/lib/run_jasmine_test.coffee src/test/javascript/TestRunner.html
+../tools/phantomjs-1.7.0-linux-x86_64/bin/phantomjs src/test/javascript/lib/run_jasmine_test.coffee src/test/javascript/TestRunner.html
