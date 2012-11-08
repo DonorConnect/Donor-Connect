1ef76e88c6dd20f7ccdcc14a59c9380716c73814
Longkai Cao <lkcao@thoughtworks.com>
2012-11-08 15:34:04 +0530

    Shari/Longkai Story #14 - add test for the ammado user input validation

--- Donor-Connect-App/src/test/javascript/AmmadoSpec.js
+++ Donor-Connect-App/src/test/javascript/AmmadoSpec.js
@@ -19,4 +19,46 @@ describe("Ammado Validations", function() {
         expect(validate("320")).toBeTruthy();
     });
 
+    it("should return false when number is greater than 570000", function() {
+        var expressionToTest = validations.maximumCustomAmountValidation.expression;
+
+        eval("var validate = function(VAL){" + expressionToTest + "}");
+
+        expect(validate("570001")).toBeFalsy();
+    });
+
+    it("should return true when number is decimal", function() {
+        var expressionToTest = validations.maximumCustomAmountValidation.expression;
+
+        eval("var validate = function(VAL){" + expressionToTest + "}");
+
+        expect(validate("500.21")).toBeTruthy();
+    });
+
+    it("should return false when number has more than two decimal", function() {
+        var expressionToTest = validations.amountMustBeNumberValidation.expression;
+
+        eval("var validate = function(VAL){" + expressionToTest + "}");
+
+        expect(validate("500.214")).toBeFalsy();
+    });
+
+    it("should return true when number is exactly 570000", function() {
+        var expressionToTest = validations.maximumCustomAmountValidation.expression;
+
+        eval("var validate = function(VAL){" + expressionToTest + "}");
+
+        expect(validate("570000")).toBeTruthy();
+    });
+
+    it("should return false when special chars are entered", function() {
+        var expressionToTest = validations.amountMustBeNumberValidation.expression;
+
+        eval("var validate = function(VAL){" + expressionToTest + "}");
+
+        expect(validate("ff")).toBeFalsy();
+        expect(validate("$1000.00")).toBeFalsy();
+    });
+
+
 });
